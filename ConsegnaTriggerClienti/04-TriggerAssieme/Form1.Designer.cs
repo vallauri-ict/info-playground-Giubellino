46209054379a285a66f785a7fa07896aaaedc108
Giubellino <m.giubellino.0730@vallauri.edu>
2021-02-17 22:57:01 +0100

    ConsegnaCompitoClienti1.0

---
+++ ConsegnaTriggerClienti/04-TriggerAssieme/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace _04_TriggerAssieme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClienti = new System.Windows.Forms.DataGridView();
+            this.dgvStoricoCancellazioni = new System.Windows.Forms.DataGridView();
+            this.dgvStoricoAggiornamenti = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnElimina = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRicaricaTabelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClienti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoCancellazioni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoAggiornamenti)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvClienti
+            // 
+            this.dgvClienti.AllowUserToAddRows = false;
+            this.dgvClienti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClienti.Location = new System.Drawing.Point(15, 30);
+            this.dgvClienti.MultiSelect = false;
+            this.dgvClienti.Name = "dgvClienti";
+            this.dgvClienti.RowHeadersVisible = false;
+            this.dgvClienti.Size = new System.Drawing.Size(443, 150);
+            this.dgvClienti.TabIndex = 0;
+            // 
+            // dgvStoricoCancellazioni
+            // 
+            this.dgvStoricoCancellazioni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStoricoCancellazioni.Location = new System.Drawing.Point(15, 225);
+            this.dgvStoricoCancellazioni.Name = "dgvStoricoCancellazioni";
+            this.dgvStoricoCancellazioni.Size = new System.Drawing.Size(586, 150);
+            this.dgvStoricoCancellazioni.TabIndex = 1;
+            // 
+            // dgvStoricoAggiornamenti
+            // 
+            this.dgvStoricoAggiornamenti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStoricoAggiornamenti.Location = new System.Drawing.Point(15, 421);
+            this.dgvStoricoAggiornamenti.Name = "dgvStoricoAggiornamenti";
+            this.dgvStoricoAggiornamenti.Size = new System.Drawing.Size(817, 150);
+            this.dgvStoricoAggiornamenti.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(499, 102);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(152, 47);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Aggiorna i dati della riga selezionata";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnElimina
+            // 
+            this.btnElimina.Location = new System.Drawing.Point(499, 30);
+            this.btnElimina.Name = "btnElimina";
+            this.btnElimina.Size = new System.Drawing.Size(100, 44);
+            this.btnElimina.TabIndex = 4;
+            this.btnElimina.Text = "Elimina riga selezionata";
+            this.btnElimina.UseVisualStyleBackColor = true;
+            this.btnElimina.Click += new System.EventHandler(this.btnElimina_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tabella Clienti";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Storico Cancellazioni";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 395);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Storico Aggiornameni";
+            // 
+            // btnRicaricaTabelle
+            // 
+            this.btnRicaricaTabelle.Location = new System.Drawing.Point(692, 30);
+            this.btnRicaricaTabelle.Name = "btnRicaricaTabelle";
+            this.btnRicaricaTabelle.Size = new System.Drawing.Size(100, 44);
+            this.btnRicaricaTabelle.TabIndex = 8;
+            this.btnRicaricaTabelle.Text = "Ricarica Tabelle";
+            this.btnRicaricaTabelle.UseVisualStyleBackColor = true;
+            this.btnRicaricaTabelle.Click += new System.EventHandler(this.btnRicaricaTabelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 581);
+            this.Controls.Add(this.btnRicaricaTabelle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnElimina);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dgvStoricoAggiornamenti);
+            this.Controls.Add(this.dgvStoricoCancellazioni);
+            this.Controls.Add(this.dgvClienti);
+            this.Name = "Form1";
+            this.Text = "Prova Trigger";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClienti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoCancellazioni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoAggiornamenti)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvClienti;
+        private System.Windows.Forms.DataGridView dgvStoricoCancellazioni;
+        private System.Windows.Forms.DataGridView dgvStoricoAggiornamenti;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnElimina;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRicaricaTabelle;
+    }
+}
---
+++ ConsegnaTriggerClienti/04-TriggerAssieme/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_TriggerAssieme
+{
+    public partial class Form1 : Form
+    {
+        public static readonly string workingDirectory = Environment.CurrentDirectory;
+        public static readonly string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        public static readonly string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + projectDirectory + @"\Clienti.mdf;Integrated Security=True;Connect Timeout=30";
+
+        private BindingSource bsClienti = new BindingSource();
+        private BindingSource bsStoricoCanc = new BindingSource();
+        private BindingSource bsStoricoUpd = new BindingSource();
+        private SqlDataAdapter daClienti, daStoricoCancellazioni, daStoricoAggiornamenti;
+        private DataTable dtClienti, dtStoricoCancellazioni, dtStoricoAggiornamenti;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateDgv(0);
+        }
+
+        private void btnRicaricaTabelle_Click(object sender, EventArgs e)
+        {
+            dgvClienti.DataSource = null;
+            dgvStoricoAggiornamenti.DataSource = null;
+            dgvStoricoCancellazioni.DataSource = null;
+            PopulateDgv(0);
+        }
+
+        private void PopulateDgv(int table)
+        {
+            switch (table)
+            {
+                case 0://carico tutte e 3 le tabelle
+                    // Clienti
+                    dgvClienti.DataSource = null;
+                    dgvClienti.DataSource = bsClienti;
+                    daClienti = null;
+                    Queryable("SELECT * FROM Clienti", out daClienti);
+
+                    if (daClienti != null)
+                    {
+                        dtClienti = new DataTable();
+                        daClienti.Fill(dtClienti);
+                        bsClienti.DataSource = dtClienti;
+                    }
+                    dgvClienti.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+                    //Cancellazioni
+                    dgvStoricoCancellazioni.DataSource = null;
+                    dgvStoricoCancellazioni.DataSource = bsStoricoCanc;
+                    daStoricoCancellazioni = null;
+                    Queryable("SELECT * FROM StoricoCancellazioni", out daStoricoCancellazioni);
+
+                    if (daStoricoCancellazioni != null)
+                    {
+                        dtStoricoCancellazioni = new DataTable();
+                        daStoricoCancellazioni.Fill(dtStoricoCancellazioni);
+                        bsStoricoCanc.DataSource = dtStoricoCancellazioni;
+                    }
+                    dgvStoricoCancellazioni.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+                    //Aggiornamenti
+                    dgvStoricoAggiornamenti.DataSource = null;
+                    dgvStoricoAggiornamenti.DataSource = bsStoricoUpd;
+                    daStoricoAggiornamenti = null;
+                    Queryable("SELECT * FROM StoricoAggiornamenti", out daStoricoAggiornamenti);
+
+                    if (daStoricoAggiornamenti != null)
+                    {
+                        dtStoricoAggiornamenti = new DataTable();
+                        daStoricoAggiornamenti.Fill(dtStoricoAggiornamenti);
+                        bsStoricoUpd.DataSource = dtStoricoAggiornamenti;
+                    }
+                    dgvStoricoAggiornamenti.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+                    break;
+
+                case 1://carico solo clienti
+                    dgvClienti.DataSource = null;
+                    dgvClienti.DataSource = bsClienti;
+                    daClienti = null;
+                    Queryable("SELECT * FROM Clienti", out daClienti);
+
+                    if (daClienti != null)
+                    {
+                        dtClienti = new DataTable();
+                        daClienti.Fill(dtClienti);
+                        bsClienti.DataSource = dtClienti;
+                    }
+                    dgvClienti.Update();
+                    dgvClienti.Refresh();
+                    dgvClienti.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+                    break;
+
+                case 2://carico solo cancellazioni
+                    dgvStoricoCancellazioni.DataSource = null;
+                    dgvStoricoCancellazioni.DataSource = bsStoricoCanc;
+                    daStoricoCancellazioni = null;
+                    Queryable("SELECT * FROM StoricoCancellazioni", out daStoricoCancellazioni);
+
+                    if (daStoricoCancellazioni != null)
+                    {
+                        dtStoricoCancellazioni = new DataTable();
+                        daStoricoCancellazioni.Fill(dtStoricoCancellazioni);
+                        bsStoricoCanc.DataSource = dtStoricoCancellazioni;
+                    }
+                    dgvStoricoCancellazioni.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+                    break;
+
+                case 3://carico solo aggiornamenti
+                    dgvStoricoAggiornamenti.DataSource = null;
+                    dgvStoricoAggiornamenti.DataSource = bsStoricoUpd;
+                    daStoricoAggiornamenti = null;
+                    Queryable("SELECT * FROM StoricoAggiornamenti", out daStoricoAggiornamenti);
+
+                    if (daStoricoAggiornamenti != null)
+                    {
+                        dtStoricoAggiornamenti = new DataTable();
+                        daStoricoAggiornamenti.Fill(dtStoricoAggiornamenti);
+                        bsStoricoUpd.DataSource = dtStoricoAggiornamenti;
+                    }
+                    dgvStoricoAggiornamenti.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+                    break;
+            }
+        }
+
+        private void Queryable(string selectCommand, out SqlDataAdapter da)
+        {
+            da = null;
+            try
+            {
+                // Crea un nuovo Data Adapter basato su selectCommand
+                da = new SqlDataAdapter(selectCommand, CONNECTION_STRING);
+                // Creo il command builder per generare update, insert, delete
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(da);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int index = dgvClienti.CurrentCell.RowIndex;//basandomi sulla cella selezionata ricavo index della riga (posso selezionarne una alla volta)
+            daClienti = null;
+            string query = "UPDATE Clienti SET Nome = '" + dgvClienti.Rows[index].Cells[1].Value.ToString() +
+                        "', Cognome = '" + dgvClienti.Rows[index].Cells[2].Value.ToString() +
+                        "', IdCarrello = " + dgvClienti.Rows[index].Cells[3].Value.ToString() +
+                        " WHERE IdCliente = " + dgvClienti.Rows[index].Cells[0].Value.ToString();
+            MessageBox.Show(query);
+            Queryable(query, out daClienti);
+
+            if (daClienti != null)
+            {
+                PopulateDgv(0);
+                //PopulateDgv(3);//aggiorno tabella update
+            }
+            else
+                MessageBox.Show("Errore Update");
+
+        }
+
+        private void btnElimina_Click(object sender, EventArgs e)
+        {
+            int index = dgvClienti.CurrentCell.RowIndex;//basandomi sulla cella selezionata ricavo index della riga (posso selezionarne una alla volta)
+            daClienti = null;
+            string query = "DELETE FROM Clienti WHERE IdCliente = " + dgvClienti.Rows[index].Cells[0].Value.ToString();
+            MessageBox.Show(query);
+            Queryable(query, out daClienti);
+            if (daClienti != null)
+            {
+                PopulateDgv(0);
+                //PopulateDgv(2);//aggiorno tabella cancellazioni
+                //PopulateDgv(1);//aggiorno tabella clienti
+                //dgvClienti.Rows.RemoveAt(index);
+            }
+            else
+                MessageBox.Show("Errore Elimina");
+
+        }
+
+    }
+}
